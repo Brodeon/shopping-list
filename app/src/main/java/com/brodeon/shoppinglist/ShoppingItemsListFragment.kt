1ca2b15d1e78fa64ec7b81fdb8b33299dd016c2d
Brodeon <Brodoen@gmail.com>
2018-11-17 20:01:49 +0100

    new method called hideFab() added.

--- app/src/main/java/com/brodeon/shoppinglist/ShoppingItemsListFragment.kt
+++ app/src/main/java/com/brodeon/shoppinglist/ShoppingItemsListFragment.kt
@@ -2,31 +2,39 @@ package com.brodeon.shoppinglist
 
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.brodeon.shoppinglist.AddEditDialog.Companion.ADD_ITEM_DIALOG_ID
 import com.brodeon.shoppinglist.data.ShoppingListItem
 import com.brodeon.shoppinglist.data.ShoppingListItemViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_shopping_items_list.*
 
-class ShoppingItemsListFragment : Fragment(), ShoppingItemsRVAdapter.OnItemLongClick {
+class ShoppingItemsListFragment : Fragment(), ShoppingItemsRVAdapter.OnItemLongClick, AddEditDialog.OnDialogResponse {
 
     private lateinit var shoppingItemsAdapter: ShoppingItemsRVAdapter
     private lateinit var itemsViewModel: ShoppingListItemViewModel
+    private var listId: Int? = null
 
     companion object {
         const val LIST_ID: String = "listId"
+        const val LIST_NAME: String = "listName"
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
+
+        this.listId = arguments?.getInt(LIST_ID)
+        val listName = arguments?.getString(LIST_NAME)
+
+        (activity as AppCompatActivity).supportActionBar?.title = listName
         return inflater.inflate(R.layout.fragment_shopping_items_list, container, false)
     }
 
@@ -36,8 +44,6 @@ class ShoppingItemsListFragment : Fragment(), ShoppingItemsRVAdapter.OnItemLongC
         val shoppingItemsRV = rv_shopping_items_list
         shoppingItemsAdapter = ShoppingItemsRVAdapter(this)
 
-        val listId = arguments?.getInt(LIST_ID)
-
         shoppingItemsRV.adapter = shoppingItemsAdapter
         shoppingItemsRV.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
 
@@ -46,10 +52,59 @@ class ShoppingItemsListFragment : Fragment(), ShoppingItemsRVAdapter.OnItemLongC
             shoppingItemsAdapter.updateList(it!!)
         })
 
+        hideFab()
         //TODO fab transition away the screen
+    }
+
+    private fun hideFab() {
+        val fab = activity?.fab
+        if (fab?.tag == FabState.VISIBLE) {
+            fab.animate().translationYBy(250f).duration = 250
+            fab.tag = FabState.HIDDEN
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.list_items_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.add_item_menu_item -> {
+                val addEditDialog = AddEditDialog()
+
+                val bundle = Bundle()
+                bundle.putInt(AddEditDialog.DIALOG_ID, AddEditDialog.ADD_ITEM_DIALOG_ID)
+                addEditDialog.arguments = bundle
+
+                addEditDialog.attachFragment(this)
+                addEditDialog.show(activity?.supportFragmentManager, null)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onItemLongClicked(position: Int, shoppingListItem: ShoppingListItem) {
         print(position)
+    }
+
+    override fun onPositiveClicked(dialogId: Int?, bundle: Bundle?) {
+        when(dialogId) {
+            ADD_ITEM_DIALOG_ID -> {
+                val itemName = bundle?.getString(AddEditDialog.ITEM_NAME_ADD_DIALOG)
+                Log.d("ItemsFragment", "item name = $itemName")
+                itemsViewModel.insertShoppingListItem(ShoppingListItem(itemName!!, false, listId!!))
+            }
+        }
+    }
+
+    override fun onNegaiveClicked(dialogId: Int?, bundle: Bundle?) {
+        when(dialogId) {
+            ADD_ITEM_DIALOG_ID -> {
+                //do nothing
+            }
+        }
     }
 }
