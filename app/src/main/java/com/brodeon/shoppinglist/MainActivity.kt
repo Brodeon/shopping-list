5b2244e057e8d514480ada658577242cb20ebace
Brodeon <Brodoen@gmail.com>
2018-11-17 19:57:47 +0100

    FabState enum added and state of fab set to visible for tag initializing

--- app/src/main/java/com/brodeon/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/brodeon/shoppinglist/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment
 
 import kotlinx.android.synthetic.main.activity_main.*
 
+enum class FabState {HIDDEN, VISIBLE}
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        fab.tag = FabState.VISIBLE
     }
 
 
